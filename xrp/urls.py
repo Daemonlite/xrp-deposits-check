1095aeadc687730f1c4ec3a604ad2f2803ebc8fd
Eugene <khemikal2016@gmail.com>
2023-09-10 15:34:52 +0000

    fetch deposits updated

--- xrp/urls.py
+++ xrp/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
   path('wallets_deposits/', get_deposits_by_address),
+  path('last_processed_ledger/', get_last_processed_ledger),
+  path('all_addresses/', get_all_addresses),
     
 ]
--- xrp/views.py
+++ xrp/views.py
@@ -1,30 +1,27 @@
-from .models import Address, Deposits
+from .models import Address, Deposits,LastProcessedLedger
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.views.decorators.http import require_GET
 
 
+
 @csrf_exempt
 def get_deposits_by_address(request):
     data = json.loads(request.body)
-    if request.method == "GET":
-        address = data["address"]
-        deposits = Deposits.objects.filter(address=address)
+    address = data["address"]
+    deposits = Deposits.objects.filter(address=address).values()
+    return JsonResponse(list(deposits), safe=False)
+    
+@csrf_exempt
+def get_last_processed_ledger(request):
+    last_processed_ledger = LastProcessedLedger.objects.get().ledger
+    return JsonResponse({"last_processed_ledger": last_processed_ledger})
 
-        deposits_list = []
-        for deposit in deposits:
-            deposit_data = {
-                "address": deposit.address,
-                "sender_address": deposit.sender_address,
-                "amount": deposit.amount,
-                "fiat_amount":f"{deposit.amount_fiat} USD",
-                "coin": deposit.coin,
-                "confirmed": deposit.confirmed,
-                "txid": deposit.txid,
-            }
-            deposits_list.append(deposit_data)
 
-        return JsonResponse({"deposits": deposits_list})
-    else:
-        return JsonResponse({"error": "This endpoint only accepts GET requests."})
+
+@csrf_exempt
+def get_all_addresses(request):
+    addresses = Address.objects.values()  # Fetch all addresses as a list of dictionaries
+
+    return JsonResponse(list(addresses), safe=False)
