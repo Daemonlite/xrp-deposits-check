27194aec78363e43dec613d2d2f6a75ce78d1194
Eugene <khemikal2016@gmail.com>
2023-10-19 15:16:57 +0000

    added signals to listen to migrations

--- xrp/apps.py
+++ xrp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class XrpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'xrp'
+
+    def ready(self):
+        import xrp.signals  
